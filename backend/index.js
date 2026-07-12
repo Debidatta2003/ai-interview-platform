405b7c5b33a40d2cc3c1b68ea965665e360ffaee
debidatta mishra <163342875+Debidatta2003@users.noreply.github.com>
2026-07-12 12:49:14 +0530

    Update CORS origin to production URL

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ import interviewRouter from "./routes/interview.route.js";
 import paymentRouter from "./routes/payment.route.js";
 const app = express();
 app.use(cors({
-  origin : "http://localhost:5173",
+  origin : "https://ai-interview-platform-1-58l6.onrender.com",
   credentials:true
 }))
 app.use(express.json())
